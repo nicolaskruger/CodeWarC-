e304102e3dff2ff93e24a1a46da8867c07671585
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-07 14:39:10 -0300

    001

--- Program.cs
+++ Program.cs
@@ -4,9 +4,26 @@ namespace codeWar
 {
     class Program
     {
+        public static int SomaString(string str){
+            int soma = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                soma+=int.Parse(str[i].ToString());
+            }
+            return soma;
+        }
+         public static int DigitalRoot(long n)
+        {
+          // Your awesome code here!
+          int val= SomaString(n.ToString());
+          if(val>9){
+              return DigitalRoot(val);
+          }
+          return val;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DigitalRoot(111111);
         }
     }
 }
