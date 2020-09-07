4ac88bc26cf40ab77adc7e6a581c20c9f523b1ea
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-07 16:57:21 -0300

    001

--- Program.cs
+++ Program.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 namespace codeWar
 {
-    class Program
+    static class Program
     {
-        public static int DuplicateCount(string str)
+        public static void print<T>(this T[] arr){
+            foreach (var a in arr)
+            {
+                System.Console.WriteLine(a);
+            }
+        }
+        public static string Longest(string s1, string s2)
         {
-            return str.ToLower().GroupBy(c=>c).Where(c=>c.Count()>1).Count();
+            s1+=s2;
+            char[] ss= s1.OrderBy(c=>c).Distinct().ToArray();
+            return string.Join("",ss);
         }
         static void Main(string[] args)
         {
-            int n= DuplicateCount("aabBcde");
-            Console.WriteLine(n);
+            string st = Longest("xyaabbbccccdefww","xxxxyyyyabklmopq");
+            System.Console.WriteLine(st);
         }
     }
 }
