d19623a8d587f0c8780d9e575cddebd4523049d5
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-07 15:17:38 -0300

    001

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
+using System.Linq;
 namespace codeWar
 {
-    class Program
+    static class Program
     {
+        public static void print<T>(this T[] array){
+            foreach (var a in array)
+            {
+                Console.WriteLine(a);
+            }
+        }
+        public static int sumTwoSmallestNumbers(params int[] numbers)
+	    {
+	    	//Code here...
+            var li = numbers.OrderBy(n=>n).ToArray();
+            return li[0]+li[1];
+	    }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            sumTwoSmallestNumbers(10, 343445353, 3453445, 45353453);
         }
     }
 }
