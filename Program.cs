28b15a1d3ef54117481c1bb920804ba961fc45d4
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-07 14:25:19 -0300

    000

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codeWar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
