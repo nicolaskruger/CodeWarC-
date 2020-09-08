a4f73f87fef9fcc4f36f37b23066f55c9304b071
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-08 13:55:23 -0300

    002

--- Program.cs
+++ Program.cs
@@ -5,26 +5,26 @@ namespace codeWar
 {
     static class Program
     {
-        public static void print<T>(this T[] arr){
-            foreach (var a in arr)
-            {
-                System.Console.WriteLine(a);
-            }
-        }
+        
         public static string Longest(string s1, string s2)
         {
             s1+=s2;
             char[] ss= s1.OrderBy(c=>c).Distinct().ToArray();
             return string.Join("",ss);
         }
+        public static void print<T>(this T[] lis){
+            System.Console.WriteLine(string.Join("\n",lis));
+        }
         public static char FindMissingLetter(char[] array)
         {
-          for (int i = 0; i < array.Length; i++)
-          {
-              if(array[i]!=(array[i+1]-1)){
-                  return (char)(array[i]+1);
-              }
-          }
+          var combo = from a in array
+                      from b in array
+                      select new{
+                          a,
+                          b
+                      };
+          combo=  combo.Where((c)=>((c.a<c.b)&&c.a==(c.b-1)));
+            combo.ToArray().print();
           return ' ';
         }
         static void Main(string[] args)
