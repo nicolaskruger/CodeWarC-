3b03ecd5b9a81185dc0d327a224c5eeece494ece
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-15 13:16:33 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -293,76 +293,33 @@ namespace codeWar
         }
         public static long NextSmaller(long n)
         {
-            var ch =n.ToString().ToArray();
-            var vall = GeneratePermutations(ch,null).Where(c =>c.ToArray()[0]!='0').
-                        Select(c =>long.Parse(string.Join("",c.ToArray()))).
-                        OrderBy(c => c).ToArray();
-            try
+            var N = n.ToString();
+            for (int i = N.Length-2; i >=0; i--)
             {
-                var val = vall.Where(c => c<n).Last();
-                return val;    
-            }
-            catch (System.Exception)
-            {
+                try
+                {
+                    string sub = N.Substring(i+1);
+                    char d = sub.Where(c => c<N[i]).OrderBy(c => c).Last();
+                    int index = sub.IndexOf(d)+1+i;
+                    char [] ch =N.ToArray();
+                    ch.swap(i,index);
+                    if(ch[0]=='0') throw new Exception();
+                    List<char> li1 = ch.Take(i+1).ToList();
+                    List<char> li2 = ch.Skip(i+1).OrderByDescending(c=>c).ToList();
+                    li1= li1.Concat(li2).ToList();
+                    return long.Parse(string.Join("",li1));
+                }
+                catch (System.Exception)
+                {
+                    
+                }
                 
-                return -1;
             }
             
+            return -1;
+            
         }
-       public static IList<T[]> GeneratePermutations<T>(T[] objs, long? limit)
-    {
-        var result = new List<T[]>();
-        long n = Factorial(objs.Length);
-        n = (!limit.HasValue || limit.Value > n) ? n : (limit.Value);
-
-        for (long k = 0; k < n; k++)
-        {
-            T[] kperm = GenerateKthPermutation<T>(k, objs);
-            result.Add(kperm);
-        }
-
-        return result;
-    }
-
-    public static T[] GenerateKthPermutation<T>(long k, T[] objs)
-    {
-        T[] permutedObjs = new T[objs.Length];
-
-        for (int i = 0; i < objs.Length; i++)
-        {
-            permutedObjs[i] = objs[i];
-        }
-        for (int j = 2; j < objs.Length + 1; j++)
-        {
-            k = k / (j - 1);                      // integer division cuts off the remainder
-            long i1 = (k % j);
-            long i2 = j - 1;
-            if (i1 != i2)
-            {
-                T tmpObj1 = permutedObjs[i1];
-                T tmpObj2 = permutedObjs[i2];
-                permutedObjs[i1] = tmpObj2;
-                permutedObjs[i2] = tmpObj1;
-            }
-        }
-        return permutedObjs;
-    }
-      public static long Factorial(int n)
-    {
-        if (n < 0) { throw new Exception("Unaccepted input for factorial"); }    //error result - undefined
-        if (n > 256) { throw new Exception("Input too big for factorial"); }  //error result - input is too big
-
-        if (n == 0) { return 1; }
-
-        // Calculate the factorial iteratively rather than recursively:
-
-        long tempResult = 1;
-        for (int i = 1; i <= n; i++)
-        {
-            tempResult *= i;
-        }
-        return tempResult;
-    }
+     
         static void Main(string[] args)
         {
             // int[]n= new int[]{
@@ -372,7 +329,7 @@ namespace codeWar
             // n.print();
             
             
-           System.Console.WriteLine(NextSmaller(29009));
+           System.Console.WriteLine(NextSmaller(531));
         }
     }
 }
