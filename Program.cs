a10b9752b06b552c306ac1ccdb071b5a7fd1f405
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-24 15:49:30 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -4,10 +4,18 @@ namespace codeWar
 {
     class Program
     {
-        
+        public static int NbYear(int p0, double percent, int aug, int p) {
+            int cont=0;
+            double P0 =(double)p0;
+            while(P0<p){
+                cont++;
+                P0+=(int)(P0*percent/100+aug);
+            }
+            return cont;
+        }
         static void Main(string[] args)
         {
-            
+            System.Console.WriteLine(NbYear(1000,2,50,1214));    
         }
     }
 }
