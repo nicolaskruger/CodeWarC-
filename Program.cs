4c433fb1889b05b6b08ab77aa79875f5b94b3b4a
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-01 18:41:15 -0300

    mySubmit01

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace codeWar
 {
     class Program
     {
-        
+        public static int find_it(int[] seq) 
+        {
+            Dictionary<int,int> dic = new Dictionary<int, int>();
+            foreach (var s in seq)
+            {
+                try
+                {
+                    dic[s]++;
+                }
+                catch (System.Exception)
+                {
+                    
+                    dic[s]=1;
+                }
+            }
+            return dic.Where(s => s.Value%2==1).First().Key;
+        }
         static void Main(string[] args)
         {
             
