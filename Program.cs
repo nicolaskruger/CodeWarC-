105550b409444bc9f829f8a0fcd5b400f15b4864
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-02 17:11:26 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace codeWar
 {
     class Program
     {
-        
+        public static string[] TowerBuilder(int nFloors)
+        {
+            string str = new string('*',nFloors*2-1);
+            List<string> st = new List<string>();
+            for (int i = 0; i < nFloors; i++)
+            {
+                st.Add(new string(' ',nFloors-i-1)+new string('*',i*2+1)+new string(' ',nFloors-i-1));
+            } 
+            return st.ToArray();
+        }
         static void Main(string[] args)
         {
-            
+            string[] str = TowerBuilder(4);
         }
     }
 }
