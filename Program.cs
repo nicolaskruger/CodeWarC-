0e92c70b6f73914d0c8efd25b6c19b022d745f7d
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-02 19:20:14 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-
+using System.Linq;
 namespace codeWar
 {
     class Program
     {
-        
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            int[] n = new int[]{a,b,c};
+            n= n.OrderBy(s => s).ToArray();
+            return n[2]<(n[0]+n[1]);
+        }
         static void Main(string[] args)
         {
-            
+            bool b = IsTriangle(1,1,1);
         }
     }
 }
