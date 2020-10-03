687896580aeab264c6955bb61fce64a31d63a28b
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-03 14:52:41 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@ namespace codeWar
 {
     class Program
     {
-        
+        public static long find(long n, long v=2){
+            n-=(v-1)*(v-1)*(v-1);
+            if(n==0) return v;
+            if(n<0) return -1;
+            return find(n,v+1);
+        }
+
+        public static long findNb(long m) {
+            return find(m);
+	    }
         static void Main(string[] args)
         {
             
