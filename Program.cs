88474357d5362fc81ef3f5f3e8b28422694ea1a8
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-03 11:41:15 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 
+using System.Linq;
+using System.Collections.Generic;
 namespace codeWar
 {
     class Program
     {
-        
+        public static int FindEvenIndex(int[] arr)
+        {
+          for (int i = 0; i < arr.Length; i++)
+          {
+             var add = arr.AsSpan();
+             var a = add[..i].ToArray().Sum();
+             var b = add[(i+1)..^0].ToArray().Sum();
+             if(a==b) return i;
+          }
+          return -1;
+        }
         static void Main(string[] args)
         {
-            
+            int n = FindEvenIndex(new int[] {1,2,3,4,3,2,1});
         }
     }
 }
