e2c5f87aba4eebd831250cda5a9ca690597789e4
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-04 13:06:00 -0300

    mySubmition

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
-
+using System.Linq;
+using System.Collections.Generic;
+//https://www.codewars.com/kata/55f2b110f61eb01779000053/train/csharp
 namespace codeWar
 {
     class Program
     {
-        
+        public int GetSum(int a, int b)
+        {
+          return (a + b) * (Math.Abs(a - b) + 1) / 2;
+        }
         static void Main(string[] args)
         {
             
