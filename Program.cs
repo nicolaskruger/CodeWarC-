757f0346e2261cf85450041b9f98471a9345f687
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-04 14:09:28 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@ namespace codeWar
 {
     class Program
     {
-        
+        public static string FindNeedle(object[] haystack)
+        {
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if(haystack[i] is string && haystack[i]=="needle") return $"found the needle at position {i}";
+            }
+            return "";
+        }
         static void Main(string[] args)
         {
             
