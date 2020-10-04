e799df2e08a80066607b4a161ac03dcac2e40609
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-04 14:44:07 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace codeWar
 {
     class Program
     {
-        
+        public delegate double Oper(double a, double b);
+        static Dictionary<string,Oper> oper = new Dictionary<string,Oper>(){
+            {"/",(a,b) => (a/b)},
+            {"*",(a,b) => (a*b)},
+            {"+",(a,b) => (a+b)},
+            {"-",(a,b) => (a-b)},
+        };
+        public static double basicOp(char operation, double value1, double value2)
+        {
+          return oper[operation.ToString()](value1,value2);
+        }
+
         static void Main(string[] args)
         {
             
