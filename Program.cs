5ac2086ffeb1f8ab24ae41d032efa2f8ca6d37a2
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-04 16:36:04 -0300

    mySumit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-
+using System.Linq;
 namespace codeWar
 {
     class Program
     {
-        
+        public static string ReverseWords(string str)
+        {
+        	return string.Join(" ",str.Split(" ").Select(s => string.Join("",s.Reverse())));
+        }
         static void Main(string[] args)
         {
-            
+            string rev = ReverseWords("This is an example!");
         }
     }
 }
