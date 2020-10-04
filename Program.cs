e7e9324f1db417ae462926974abc7d0956e578da
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-04 09:48:29 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace codeWar
 {
     class Program
     {
-        
+        static Regex reg = new Regex(@"^[:;][-~]?[)D]$");
+        public static int CountSmileys(string[] smileys) 
+        {
+           return smileys.Where(s => reg.IsMatch(s)).Count();
+        }
         static void Main(string[] args)
         {
-            
+            int c= CountSmileys(new string[]{":)", ";(", ";}", ":-D"});
         }
     }
 }
