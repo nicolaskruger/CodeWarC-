362afacead0e9cd94a9fa6cabbd2f7d29bc9b789
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-04 10:24:38 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 
+using System.Linq;
 namespace codeWar
 {
-    class Program
+    static class Program
     {
-        
+        public static string ToJadenCase(this string phrase)
+        {
+          return string.Join(" ",phrase.Split(" ").Select(s => (s[0].ToString().ToUpper()+s.Substring(1))));
+        }
         static void Main(string[] args)
         {
-            
+            string str = ToJadenCase("How can mirrors be real if our eyes aren't real");
         }
     }
 }
