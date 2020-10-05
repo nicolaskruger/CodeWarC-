91fdc410c6a5a74d6c3bb568935c739c5ec094d9
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-05 09:12:36 -0300

    best

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace codeWar
 {
     class Program
     {
-        
+        public static List<int> RemoveSmallest(List<int> numbers)
+  {
+    numbers.Remove(numbers.DefaultIfEmpty().Min());
+    return numbers;
+  }
         static void Main(string[] args)
         {
-            
+            var lista = RemoveSmallest(new List<int>{1, 2, 3, 4, 5});
         }
     }
 }
