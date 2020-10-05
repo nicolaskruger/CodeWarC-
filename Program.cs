d278c3705e57a477b1b289778fb9ff344d22cd3a
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-05 10:12:23 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-
+using System.Linq;
 namespace codeWar
 {
     class Program
     {
-        
+        public static string AbbrevName(string name)
+        {
+          return string.Join(".",name.Split(" ").Select(s => s[0].ToString().ToUpper()));
+        }
         static void Main(string[] args)
         {
             
