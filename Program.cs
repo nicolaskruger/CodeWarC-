e5945ecdc53d0cbb5af6fcf13bce1748971beb0c
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-05 08:10:13 -0300

    try

--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
-
+using System.Linq;
 namespace codeWar
 {
     class Program
     {
-        
+        public static string SongDecoder(string input)
+        { 
+            bool space = true;
+            return
+            string.Join("",
+            string.Join("",input.Split("WUB").Select(s=>{
+               return s+" ";
+           })).Trim().Split("").Select(s =>{
+               if(space && s==" "){
+                   space = false;
+                   return " ";
+               }
+               space = true;
+               return s;
+           }));
+           
+        }
         static void Main(string[] args)
         {
-            
+            string str =SongDecoder("AWUBBWUBCWUBD");
         }
     }
 }
