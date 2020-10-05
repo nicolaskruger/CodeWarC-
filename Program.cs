d702ed119063339d6feb32b73e146a60829175a8
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-05 13:39:31 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ namespace codeWar
 {
     class Program
     {
+        public static long FindNextSquare(long num)
+        {
+           double sqr = Math.Sqrt(num);
+           if((sqr-(long)sqr)!=0) return -1;
+           return (long)Math.Pow(sqr+1,2);
+        }
         static void Main(string[] args)
         {
             
