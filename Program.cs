292ba4945400a113f1813549c290544c73eb8064
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-05 12:21:28 -0300

    default

--- Program.cs
+++ Program.cs
@@ -4,10 +4,6 @@ namespace codeWar
 {
     class Program
     {
-        public static string Solution(string str) 
-        {
-          return string.Join("",str.Reverse());
-        }
         static void Main(string[] args)
         {
             
