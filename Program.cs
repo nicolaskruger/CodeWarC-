f8764e36172f01c6145f3bb5fe3524483bb34d81
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-05 12:27:18 -0300

    mySubmit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ namespace codeWar
 {
     class Program
     {
+      public string dnaToRna(string dna)
+      {
+        return string.Join("",dna.Select(s => {
+          if(s == 'T') return 'U';
+          return s;
+        }));
+      }
         static void Main(string[] args)
         {
             
